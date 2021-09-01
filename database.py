a82f763e835f9e0b11ba0b82be56e65c761e10e4
Renan Santana <renan.santana@instruct.com.br>
2021-09-01 13:47:00 -0300

    refactor

--- database.py
+++ database.py
@@ -7,17 +7,19 @@ from sqlalchemy.ext.declarative import declarative_base
 
 if os.environ.get("FLASK_ENV") == "development":
     database_url = 'sqlite:////tmp/test.db'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 else:
-    database_url = os.environ.get('DATABASE_URL').replace('postgres', 'postgresql')
-    # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-
+    database_url = os.environ.get('DATABASE_URL').replace(
+        'postgres', 'postgresql'
+    )
 
 engine = create_engine(database_url)
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
+db_session = scoped_session(
+    sessionmaker(
+        autocommit=False,
+        autoflush=False,
+        bind=engine
+    )
+)
 Base = declarative_base()
 Base.query = db_session.query_property()
 
